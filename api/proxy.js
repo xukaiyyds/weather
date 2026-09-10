878f17f6c587b3c7bb96626bd10a6fb0b2deea99
xukaiyyds <3353212714@qq.com>
2026-09-11 04:45:24 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
@@ -46,4 +46,4 @@ module.exports = async (req, res) => {
       message: error.message,
     });
   }
-};
+}
